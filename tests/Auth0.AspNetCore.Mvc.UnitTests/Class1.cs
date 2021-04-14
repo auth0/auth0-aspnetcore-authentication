34ce7c5ec711942bad23d3417c52163cb9a10111
Frederik Prijck <frederik.prijck@auth0.com>
2021-04-14 11:53:05 +0200

    [SDK-2489] Add dummy test to integrate CI (#5)

---
+++ tests/Auth0.AspNetCore.Mvc.UnitTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Auth0.AspNetCore.Mvc.UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Dummy()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
