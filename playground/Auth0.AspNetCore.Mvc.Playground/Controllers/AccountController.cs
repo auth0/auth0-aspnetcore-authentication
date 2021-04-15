cc8d4619642be49427397de64748807bc4bf0df7
Frederik Prijck <frederik.prijck@gmail.com>
2021-04-15 15:22:21 +0200

    Use AuthenticationScheme from Constants in playground

--- playground/Auth0.AspNetCore.Mvc.Playground/Controllers/AccountController.cs
+++ playground/Auth0.AspNetCore.Mvc.Playground/Controllers/AccountController.cs
@@ -13,13 +13,13 @@ namespace Auth0.AspNetCore.Mvc.Playground.Controllers
     {
         public async Task Login(string returnUrl = "/")
         {
-            await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+            await HttpContext.ChallengeAsync(Constants.AuthenticationScheme, new AuthenticationProperties() { RedirectUri = returnUrl });
         }
 
         [Authorize]
         public async Task Logout()
         {
-            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+            await HttpContext.SignOutAsync(Constants.AuthenticationScheme, new AuthenticationProperties
             {
                 // Indicate here where Auth0 should redirect the user after a logout.
                 // Note that the resulting absolute Uri must be whitelisted in the
