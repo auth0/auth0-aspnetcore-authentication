6893155db3b9b5e0be9e473829c4ea6b16d8df51
Frederik Prijck <frederik.prijck@gmail.com>
2021-05-19 23:14:54 +0200

    Clean up

--- tests/Auth0.AspNetCore.Mvc.IntegrationTests/Utils/JwtUtils.cs
+++ tests/Auth0.AspNetCore.Mvc.IntegrationTests/Utils/JwtUtils.cs
@@ -1,12 +1,8 @@
-﻿
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Auth0.AspNetCore.Mvc.IntegrationTests
 {
