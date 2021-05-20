4521ebdfc0faf8294bd29e0edf6639c1f1ff7230
Frederik Prijck <frederik.prijck@auth0.com>
2021-05-20 20:20:32 +0200

    Improve Integration tests (#24)

--- tests/Auth0.AspNetCore.Mvc.IntegrationTests/Utils/JwtUtils.cs
+++ tests/Auth0.AspNetCore.Mvc.IntegrationTests/Utils/JwtUtils.cs
@@ -1,17 +1,25 @@
-﻿
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Auth0.AspNetCore.Mvc.UnitTests
+namespace Auth0.AspNetCore.Mvc.IntegrationTests
 {
-    public static class JwtUtils
+    /// <summary>
+    /// Utils class to generate a JWT token for testing purposes.
+    /// </summary>
+    internal static class JwtUtils
     {
+        /// <summary>
+        /// Generate a JWT token using the provided options
+        /// </summary>
+        /// <param name="userId">The user's ID, used as Name and sub claim.</param>
+        /// <param name="issuer">The token's Issuer.</param>
+        /// <param name="audience">The token's Audience</param>
+        /// <param name="org_id">The (optional) org_id claim to be used.</param>
+        /// <param name="nonce">The (optional) nonce to be used.</param>
+        /// <returns>The generated JWT token.</returns>
         public static string GenerateToken(int userId, string issuer, string audience, string org_id = null, string nonce = null)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
