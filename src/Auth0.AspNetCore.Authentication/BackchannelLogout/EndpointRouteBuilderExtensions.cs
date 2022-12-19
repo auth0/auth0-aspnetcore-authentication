1e728c6b4692e0b487d0507924cd1c173ff75610
frederikprijck <frederik.prijck@auth0.com>
2022-12-19 16:57:38 +0100

    Move EndpointRouteBuilderExtensions into its own file

---
+++ src/Auth0.AspNetCore.Authentication/BackchannelLogout/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth0.AspNetCore.Authentication
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static void MapBackchannelEndpoint(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapPost("backchannel-logout", (context) =>
+            {
+                var handler = context.RequestServices.GetRequiredService<BackchannelLogoutHandler>();
+                return handler.HandleRequestAsync(context);
+            })
+                .AllowAnonymous();
+        }
+    }
+}
