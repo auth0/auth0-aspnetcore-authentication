3d2ef74291bc484f210ee57d843abec905b6dc8e
frederikprijck <frederik.prijck@auth0.com>
2022-12-19 16:57:28 +0100

    Move DefaultLogoutTokenHandler into own file and add warning when used

---
+++ src/Auth0.AspNetCore.Authentication/BackchannelLogout/DefaultLogoutTokenHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Auth0.AspNetCore.Authentication
+{
+    public class DefaultLogoutTokenHandler : ILogoutTokenHandler
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly ILogger logger;
+
+        public DefaultLogoutTokenHandler(IMemoryCache memoryCache, ILoggerFactory loggerFactory)
+        {
+            this.memoryCache = memoryCache;
+            this.logger = loggerFactory.CreateLogger("Auth0.DefaultLogoutTokenHandler");
+        }
+
+        public Task OnTokenReceivedAsync(string issuer, string sid, string logoutToken)
+        {
+            this.logger.LogWarning("Using back-channel logout without providing a custom ILogoutTokenHandler implementation, falling back to `DefaultLogoutTokenHandler. Be aware this store the tokens in IMemoryCache`");
+
+            // TODO: Configure expiration
+            memoryCache.Set($"{issuer}|{sid}", logoutToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> IsLoggedOutAsync(string issuer, string sid)
+        {
+            this.logger.LogWarning("Using back-channel logout without providing a custom ILogoutTokenHandler implementation, falling back to `DefaultLogoutTokenHandler. Be aware this store the tokens in IMemoryCache`");
+
+            var token = memoryCache.Get($"{issuer}|{sid}");
+            return Task.FromResult(token != null);
+        }
+
+        public Task RemoveAsync(string issuer, string sid)
+        {
+            this.logger.LogWarning("Using back-channel logout without providing a custom ILogoutTokenHandler implementation, falling back to `DefaultLogoutTokenHandler. Be aware this store the tokens in IMemoryCache`");
+
+            memoryCache.Remove($"{issuer}|{sid}");
+            return Task.CompletedTask;
+        }
+    }
+}
