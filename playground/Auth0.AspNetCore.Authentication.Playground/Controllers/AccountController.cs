69825e2e80b7fea526601e923b779136507a4ddf
kailash-b <kailash.b@okta.com>
2025-08-14 16:57:03 +0530

    Display tokens on Profile page for easier debugging

--- playground/Auth0.AspNetCore.Authentication.Playground/Controllers/AccountController.cs
+++ playground/Auth0.AspNetCore.Authentication.Playground/Controllers/AccountController.cs
@@ -67,7 +67,10 @@ public class AccountController : Controller
         {
             Name = User.Identity.Name,
             EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
-            ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
+            ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value,
+            AccessToken = accessToken,
+            IdToken = idToken,
+            RefreshToken = refreshToken
         });
     }
 
--- playground/Auth0.AspNetCore.Authentication.Playground/Models/UserProfileViewModel.cs
+++ playground/Auth0.AspNetCore.Authentication.Playground/Models/UserProfileViewModel.cs
@@ -2,11 +2,10 @@
 
 public class UserProfileViewModel
 {
-    public string Country { get; set; }
-
-    public string EmailAddress { get; set; }
-
     public string Name { get; set; }
-
+    public string EmailAddress { get; set; }
     public string ProfileImage { get; set; }
+    public string AccessToken { get; set; }
+    public string IdToken { get; set; }
+    public string RefreshToken { get; set; }
 }
