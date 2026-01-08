7a26c55c9448315cc88835dbf6e29f6f1f9c30a4
kailash-b <kailash.b@okta.com>
2026-01-08 14:21:11 +0530

    Adds Auth0CustomDomainsStartupFilter to resolve the custom domain using DomainResolver

---
+++ src/Auth0.AspNetCore.Authentication/CustomDomains/Auth0CustomDomainsOptions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Auth0.AspNetCore.Authentication.CustomDomains;
+
+public class Auth0CustomDomainsOptions
+{
+    /// <summary>
+    /// Resolves the Domain (issuer) for the current request.
+    /// </summary>
+    /// <example>
+    /// Resolves to a domain like <c>tenant.auth0.com</c> based on the request.
+    /// </example>>
+    public Func<HttpContext, Task<string>>? DomainResolver { get; set; }
+
+    /// <summary>
+    /// Enables or disables cookies per issuer.
+    /// <remarks>Defaults to <c>true</c></remarks>
+    /// </summary>
+    public bool EnableCookiesPerIssuer { get; set; } = true;
+    
+    /// <summary>
+    /// Indicates whether multiple custom domains are enabled by checking if <see cref="DomainResolver"/> is set.
+    /// </summary>
+    internal bool IsMultipleCustomDomainsEnabled => DomainResolver != null;
+}
